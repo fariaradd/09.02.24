ee84b60494c7dac5f069ed03856f3681c06f4bf4
Faria <142435808+fariaradd@users.noreply.github.com>
2024-02-09 12:16:48 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+// Definindo duas variáveis com números para comparar
+var numero1 = 10;
+var numero2 = 10;
+
+// Verificando se os números são iguais
+if (numero1 === numero2) {
+    console.log("Números conferem");
+} else {
+    console.log("Números não conferem");
+}
